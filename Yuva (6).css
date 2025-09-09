2cd13b87a646f5482a091a455d25386a2335ad1c
yuvapriya863 <yuvapriya863@gmail.com>
2025-09-09 12:15:40 +0530

    Add files via upload

---
+++ Yuva (6).css
@@ -0,0 +1,24 @@
+<!Doctype html>
+<html>
+    <head>
+        <style>
+            a {
+  color: #ff652f;
+  font-size: 2rem;
+  padding: 2rem;
+}
+
+a::before {
+  content: "\1F517 ";
+}
+a::after {
+  content: " (" attr(href) ")";
+  font-size: 1rem;
+  color: #272727;
+}
+        </style>
+    </head>
+    <body>
+        <a href="https://codeSTACKr.com">codeSTACKr</a>
+    </body>
+</html>
